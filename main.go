99a82e49261f2a11cfc2e222abed66e6cfacac81
Filipe Pina <fopina@gmail.com>
2022-07-16 00:21:13 +0100

    goreleaser

--- main.go
+++ main.go
@@ -19,12 +19,15 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
+const Version = "dev"
+
 // top level for easier testing
 var bindAddress = flag.StringP("bind", "b", "127.0.0.1:8081", "address:port to bind webserver")
 var iptablesPath = flag.StringP("iptables-bin", "i", "/usr/sbin/iptables", "Path to iptables")
 var iptablesSavePath = flag.StringP("iptables-save-bin", "s", "/sbin/iptables-save", "Path to iptables-save")
 var clientIp = flag.StringP("client-ip", "c", "192.168.0.222", "Client IP that should be redirected")
 var ipHeader = flag.StringP("ip-header", "x", "", "header for user IP, such as X-Real-IP or X-Forwarded-For - this is NOT used for security, it's only for displaying remote IP in the UI")
+var version = flag.Bool("version", false, "display version")
 
 var iptablesParser = regexp.MustCompile(`-A PREROUTING -s (\d+\.\d+\.\d+\.\d+)/32 -p tcp -m tcp --dport (\d+) -j DNAT --to-destination (\d+\.\d+\.\d+\.\d+):(\d+)`)
 
@@ -184,6 +187,10 @@ func runIt() error {
 
 func main() {
 	flag.Parse()
+	if *version {
+		fmt.Println(Version)
+		return
+	}
 	err := runIt()
 	if err != nil {
 		log.Fatal(err)
